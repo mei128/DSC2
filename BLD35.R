ad6713231f8e2b7aca15375b891c80814e86be88
MEI <mei128@infantes.com>
2019-10-21 13:49:50 +0200

    Commented out stopword removal. Let's see...

--- BLD35.R
+++ BLD35.R
@@ -17,15 +17,17 @@ if (!exists("tokens_full")) {                 # Load where we left if unchained 
 
 tokens_full <- tokens_sample(tokens_full,chnknum*chnksize) # subsample of similar total size
 
-tokens_full <- tokens_remove(tokens_full, c(stopwords(),"")) # remove stopwords and padding
+#tokens_full <- tokens_remove(tokens_full, c(stopwords(),"")) # remove stopwords and padding
+tokens_full <- tokens_remove(tokens_full, "") # remove padding
 
 fcmc <- fcm(tokens_full,tri = FALSE)          # Symmetric co-ocurrence matrix
 fcmc <- as.matrix(fcmc)
 message("Co-occurence matrix done.")
 write_rds(fcmc,dpath_fcm)                     # Save fcm
-message("Saved matrix (count)")
+message("Saved matrix (x count)")
 
 tknc <- apply(fcmc,1,sum)                     # Individual token count
+message("Individual token counts.")
 d    <- dim(fcmc)[1]
 for (i in 1:d)
     for (j in 1:d) fcmc[i,j]<-fcmc[i,j]/(tknc[i]+tknc[j])
